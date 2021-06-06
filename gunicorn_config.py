6f6c077c90ec730cb79cd1f9f47f9243563637f3
Andre Emidio <andresjc2008@gmail.com>
2021-06-05 21:31:50 -0300

    Configuração do Gunicorn

---
+++ gunicorn_config.py
@@ -0,0 +1,15 @@
+worker_class = 'gevent'
+worker_connections = 25
+workers = 4
+bind = 'unix:///tmp/nginx.socket'
+
+def when_ready(server):
+    open('/tmp/app-initialized', 'w').close()
+
+def pre_fork(server, worker):
+    import gevent.monkey
+    import psycogreen.gevent
+    gevent.monkey.patch_all()
+    print('Making code green')
+    psycogreen.gevent.patch_psycopg()
+    print('Making postgres green')
