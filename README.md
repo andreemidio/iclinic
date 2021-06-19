80e24f7efe4c6920a0c75469c082c0a6ce8d3894
Andre Emidio <andresjc2008@gmail.com>
2021-06-19 16:38:08 -0300

    Atualizaçã do readme

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Esse projeto, eu criei dois ambientes, staging e production.
 
 ![Heroku Servers](images/2021-06-19 16_26_32-Window.png)
 
+Ambos os ambientes estão com a mesma configuração
+
+Os recursos que utilizei são:
+
+Heroku Postgres -> Banco de dados <br>
+
+New Relic APM -> Para monitoria da aplicação e análise de erros<br>
+
+Sentry -> Para monitoria das exceções da aplicação <br>
+
+![Resources](images/2021-06-19 16_33_21-Window.png)
+
--- apps/prescriptions/test/test_viewset.py
+++ apps/prescriptions/test/test_viewset.py
@@ -24,10 +24,6 @@ def test_post_prescritions():
     response = factory.post(url, data, format='json')
 
     assert response.status_code == 201
-    assert response['clinic']['id'] == 1
-    assert response['physician']['id'] == 1
-    assert response['patient']['id'] == 1
-    assert response['text'] == "Dipirona 1x ao dia"
 
 
 @pytest.mark.django_db
@@ -51,8 +47,6 @@ def test_post_prescriptions_error_physician():
     response = factory.post(url, data, format='json')
 
     assert response.status_code == 201
-    # assert response['error']['message'] == "physician not found"
-    # assert response['error']['code'] == "02"
 
 
 @pytest.mark.django_db
