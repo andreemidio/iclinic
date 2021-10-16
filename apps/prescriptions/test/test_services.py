323769a394ca937986490e6f7e341606512f6079
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2021-10-16 18:17:20 -0300

    Django Update e test integration

--- apps/prescriptions/test/test_services.py
+++ apps/prescriptions/test/test_services.py
@@ -18,7 +18,7 @@ def test_patients_sucess():
 
 
 def test_patients_not_found():
-    data = request_endpoint.patients(id=50)
+    data = request_endpoint.patients(id=100)
 
     assert data['error']['message'] == "patient not found"
     assert data['error']['code'] == "03"
@@ -27,8 +27,8 @@ def test_patients_not_found():
 def test_physicians_sucess():
     data = request_endpoint.physicians(id=1)
 
-    assert data['name'] == 'Eduardo Wiza MD'
-    assert data['crm'] == 'de7099b8-f56d-480e-ab6d-6257773063e6'
+    assert data['name'] == 'Mrs. Janie Schuppe'
+    assert data['crm'] == '871ab521-13e9-4a8f-9f35-a03641ac652a'
 
 
 def test_physicians_not_found():
@@ -42,11 +42,11 @@ def test_metrics():
     data = request_endpoint.metrics_request(clinics_id=1, patients_id=1, physicians_id=1)
 
     assert data["clinic_id"] == "1"
-    assert data["clinic_name"] == "Dr. Kennith Bednar"
+    assert data["clinic_name"] == "Shaun Ledner"
     assert data["physician_id"] == "1"
-    assert data["physician_name"] == "Eduardo Wiza MD"
-    assert data["physician_crm"] == "de7099b8-f56d-480e-ab6d-6257773063e6"
+    assert data["physician_name"] == "Mrs. Janie Schuppe"
+    assert data["physician_crm"] == "871ab521-13e9-4a8f-9f35-a03641ac652a"
     assert data["patient_id"] == "1"
-    assert data["patient_email"] == "Muriel4@gmail.com"
-    assert data["patient_phone"] == "1-853-068-7209 x379"
-    assert data["patient_name"] == "Walter Ziemann"
+    assert data["patient_email"] == "Ross49@gmail.com"
+    assert data["patient_phone"] == "(263) 369-7435 x40648"
+    assert data["patient_name"] == "Claude Funk"
