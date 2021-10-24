8b0eacc17afc3269fff8c280e41895fc4d8c7262
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2021-10-23 22:22:12 -0300

    Implementação do celery

---
+++ config/celery.py
@@ -0,0 +1,23 @@
+import os
+
+from celery import Celery
+# Set the default Django settings module for the 'celery' program.
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('iclinic')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+
+app = Celery('iclinic',
+             broker="amqp://user:bitnami@localhost:5672/myvhost",
+             include=['config.tasks'])
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
